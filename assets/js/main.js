51879c52ad74274a7705dbe5f554865ffad1afa0
Claudio <claudiocarchietti@yahoo.it>
2021-03-05 16:10:06 +0100

    Esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 // PROVA FINALE
-for (var i = 1; i < 101; i++) {
-  if (i %15 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "FIzzBuzz" + "</li>";
+for (var i = 1; i <= 100; i++) {
+  if (i %3 == 0 && i %5!=0) {
+    document.getElementById('numeri').innerHTML += "<li>" + "FIzz" + "</li>";
   }
-  else if (i %5 == 0){
+  else if (i %5 == 0 && i %3!=0){
     document.getElementById('numeri').innerHTML += "<li>" + "Buzz" + "</li>";
   }
-  else if (i %3 == 0){
-    document.getElementById('numeri').innerHTML += "<li>" + "Fizz" + "</li>";
+  else if (i %3 == 0 && i%5 ==0){
+    document.getElementById('numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
   }
   else{
     document.getElementById('numeri').innerHTML += "<li>" + i + "</li>";
